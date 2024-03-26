f6e519254d675d7424965d296c40a6ec426ad5dc
Vbrawl <greninjagr@gmail.com>
2024-03-26 12:56:45 +0200

    Fixed a relative path.

--- build/make-exe.bat
+++ build/make-exe.bat
@@ -1,5 +1,5 @@
 echo "Compiling the UI file..."
-pyside6-uic ../mainwindow.ui -o ui_mainwindow.py
+pyside6-uic ../mainwindow.ui -o ../ui_mainwindow.py
 
 echo "Compiling the onefile..."
 pyinstaller --noconfirm --onefile --windowed --name ChatGPTChatRoom --icon ../logo.png --add-data ../logo.png:. ../MainWindow.py
